7dfb61febea49fd57bd054cea8d28df31d1fb986
Taylor Wu <taylorwu21@gmail.com>
2017-06-29 11:15:14 -0600

    updated auto liker to take in user login info

--- auto_liker.rb
+++ auto_liker.rb
@@ -2,17 +2,20 @@ require "watir"
 require "pry"
 require "rb-readline"
 require "awesome_print"
-require_relative "credentials"
+require "highline/import"
 
-username = $username
-password = $password
+puts `clear`
+puts "🔥🔥🔥🔥🔥 \s TAYLOR'S DOPE ASS INSTAGRAM AUTO LIKER \s 🔥🔥🔥🔥🔥"
+
+username = ask("Enter your Username?  ")
+password = ask("Enter your password:  ") { |q| q.echo = "*" }
 like_counter = 0
 num_of_rounds = 0
 
 browser = Watir::Browser.new :chrome, switches: ["--incognito"]
 browser.goto "https://www.instagram.com/accounts/login/"
 
-ap "Loggin in..."
+ap "Loggin in for you 😘"
 
 browser.text_field(name: "username").set "#{username}"
 browser.text_field(name: "password").set "#{password}"
@@ -21,23 +24,31 @@ browser.button(class: "_ah57t _84y62 _i46jh _rmr7s").click
 sleep(2)
 
 while true
-  3.times do |i|
+  5.times do |i|
     browser.driver.execute_script("window.scrollBy(0, document.body.scrollHeight)")
-    sleep(1)
+    sleep(2)
   end
 
-  if browser.span(class: "coreSpriteHeartOpen").exists?
-    browser.spans(class: "coreSpriteHeartOpen").each { |val| 
+  if browser.span(class: "coreSpriteLikeHeartOpen").exists?
+    browser.spans(class: "coreSpriteLikeHeartOpen").each { |val|
       val.click
       like_counter += 1
+      sleep(1)
     }
-    ap "Photos Liked: #{like_counter}"
+    ap "❤️\s count: #{like_counter}"
+  elsif browser.span(class: "coreSpriteHeartOpen").exists?
+    browser.spans(class: "coreSpriteHeartOpen").each { |val|
+      val.click
+      like_counter += 1
+      sleep(1)
+    }
+    ap "❤️\s count: #{like_counter}"
   else
     ap "No media to like yo, sorry homie"
-    exit
+    # exit
   end
   num_of_rounds += 1
-  puts "=======#{like_counter / num_of_rounds} likes per round (on average) =========="
+  puts "======= #{like_counter / num_of_rounds} ❤️ \s per round (on average) =========="
   sleep(30)
 end
 
